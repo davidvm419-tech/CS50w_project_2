a334cdd42057749027bada913670a809da0b52c3
Juan David <davidvm419@gmail.com>
2026-02-22 10:38:40 -0500

    Added models, index template and index view.

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,43 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+# Fill the default class parameters, not custom values for now
 class User(AbstractUser):
     pass
+
+
+class AuctionListing(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings"    )
+    category = models.CharField(max_length= 64)
+    title = models.CharField(max_length= 64)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Bid(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    product = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+class AuctionComment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    product = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="comments")
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+class WatchList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlist")
+    product = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="watchlist_by")
+
+    # Constrain user to only add the same product once to the watchlist avoiding not necessary duplicates
+    class Meta:
+        # This is the correct name variable
+        unique_together = ("user", "product")
+        
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import AuctionListing, AuctionComment, Bid, User, WatchList
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    auction_list = AuctionListing.objects.all()
+    return render(request, "auctions/index.html", {
+        "auction_list": auction_list,
+    })
 
 
 def login_view(request):
